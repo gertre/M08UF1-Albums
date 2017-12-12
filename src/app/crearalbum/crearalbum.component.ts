60093b2c3226bc8cc418d38a266c3028fe39e003
UVIC\avillar <adriavillar96@gmail.com>
2017-12-12 17:50:55 +0100

    no message

--- src/app/crearalbum/crearalbum.component.ts
+++ src/app/crearalbum/crearalbum.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumsService } from "../albums.service";
 
 @Component({
   selector: 'app-crearalbum',
   templateUrl: './crearalbum.component.html',
-  styleUrls: ['./crearalbum.component.css']
+  styleUrls: ['./crearalbum.component.css'],
+  providers: [AlbumsService]
 })
 export class CrearalbumComponent implements OnInit {
 
